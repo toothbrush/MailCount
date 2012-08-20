d573cebc6ef26eaa39ea0ea735723c6fde3c1d87
Paul van der Walt <paul@denknerd.org>
2012-08-20 20:27:16 +0200

    tests, unused as yet

---
+++ MailCountTests/MailCountTests.h
@@ -0,0 +1,13 @@
+//
+//  MailCountTests.h
+//  MailCountTests
+//
+//  Created by Paul van der Walt on 19/08/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface MailCountTests : SenTestCase
+
+@end
